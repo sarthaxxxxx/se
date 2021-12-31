ebac5863ceb3f4ecd03a94d7d4bb33a8a043d18c
Sarthak Maharana <maharana@usc.edu>
2021-12-31 01:56:21 -0800

    no val

--- src/runner.py
+++ src/runner.py
@@ -8,6 +8,6 @@ import torch.backends.cudnn as cudnn
 import numpy as np
 from tqdm import tqdm 
 
-def trainer(train_loader, cfg, epoch, device, val_loader = None):
+def trainer(train_loader, cfg, epoch, device):
     for idx, (clean, noise) in tqdm(enumerate(train_loader)):
         pass
